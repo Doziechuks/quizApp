2dc437da9e045891ccf6c39296777a2364c721cf
doziechuks <doziechuks1010@gmail.com>
2022-12-12 15:54:33 +0100

    i created the question page

--- src/components/customButton.jsx
+++ src/components/customButton.jsx
@@ -1,8 +1,8 @@
 import classes from './customButton.module.css';
 
-const CustomButton = ({ children, isSettings, ...otherProps }) => {
+const CustomButton = ({ children, isSettings, isQuestion, ...otherProps }) => {
   return ( 
-    <button className={`${classes.button} ${isSettings ? classes.settings : ''}`} { ...otherProps }>{children}</button>
+    <button className={`${classes.button} ${isSettings ? classes.settings : ''} ${isQuestion ? classes.question : ''}`} { ...otherProps }>{children}</button>
    );
 }
  
--- src/pages/questions.jsx
+++ src/pages/questions.jsx
@@ -1,11 +1,22 @@
+import CustomButton from '../components/customButton';
 import classes from './questions.module.css';
 
 const QuestionsPage = () => {
-  return ( 
-    <div>
-      <h1>questions</h1>
+  return (
+    <div className={classes.wrapper}>
+      <h1>question 1</h1>
+      <div className={classes.container}>
+        <p className={classes.question}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quo pariatur obcaecati minima praesentium nobis accusamus error vero aspernatur modi quos, soluta ducimus? Dicta deleniti doloremque quo corrupti asperiores in!</p>
+        <div className={classes.options}>
+          <CustomButton isQuestion>option a</CustomButton>
+          <CustomButton isQuestion>option a</CustomButton>
+          <CustomButton isQuestion>option a</CustomButton>
+          <CustomButton isQuestion>option a</CustomButton>
+        </div>
+      </div>
+      <p className={classes.score}>score: 1 / 6</p>
     </div>
-   );
+  );
 }
  
 export default QuestionsPage;
